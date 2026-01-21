6c9a8d9ffc05bf2f3d82ce4bf40a35845c8f192a
Lhakpa Sherpa <ls3882n@gre.ac.uk>
2026-01-21 03:31:19 +0000

    Loading Dataset

---
+++ DataProcessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split#
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+# Load dataset
+raw_path = "Carbon Emission.csv"
+
+columns = [
+    "BodyType", "Sex", "Diet", "HowOftenShower", "HeatingEnergySource",
+    "Transport", "VehicleType", "SocialActivity", "MonthlyGroceryBill",
+    "FrequencyTravelAir", "VehicleMonthlyDistanceKm", "WasteBagSize",
+    "WasteBagWeeklyCount", "HowLongTVPCDailyHour", "HowManyNewClothesMonthly",
+    "HowLongInternetDailyHour", "EnergyEfficiency", "Recycling",
+    "CookingWith", "CarbonEmission"
+]
+
+df = pd.read_csv(raw_path)
+
+print(df.head())
+print(df.shape)
+print(df.columns)
+
